4d2d2d1d77509c95f21d5da398304bdc0149f1f5
0BL1V10N <cvjeticaxel@gmail.com>
2025-04-22 20:01:51 +0200

    feat: add default controller

---
+++ tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+final class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex(): void
+    {
+        $client = self::createClient();
+        $client->request('GET', '/default');
+
+        self::assertResponseIsSuccessful();
+    }
+}
