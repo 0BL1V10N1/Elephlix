d74f9b04299e14694854f8c30f12ad4e147a5d3e
0BL1V10N <cvjeticaxel@gmail.com>
2025-05-05 20:18:57 +0200

    chore(docker): add docker config for prod
    - added a docker config for prod
    - added optional installation for sentry
    - renamed a test file

--- tests/Controller/PageControllerTest.php
+++ tests/Controller/PageControllerTest.php
@@ -4,7 +4,7 @@ namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-final class DefaultControllerTest extends WebTestCase
+final class PageControllerTest extends WebTestCase
 {
     public function testIndex(): void
     {
